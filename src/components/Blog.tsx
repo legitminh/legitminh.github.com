daf7588329800d67973e6046180b4d090cc269f5
totallylegitperson.example@gmial.com <totallylegitperson.example@gmial.com>
2023-04-30 21:24:43 -0400

    feat: make blog looks good

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -15,44 +15,48 @@ export default function f( props : {
     
     return (
         <>
-        <div className = "box"
-         style={{
-            display: "flex",
-            // position: 'absolute',
-            // // left: '1rem',
-            // bottom: '2rem',
-            // padding: '0.5rem',
-            // fontFamily: 'sans-serif',
-            borderRadius: '0.5rem',
-            // width:"100%-1rem",
-            height: "10rem",
-            // fontSize: '1.5rem',
-            
-        }} onClick={link}>
-            <div>
-                {title}
-                {ct}
-            </div>
-            <div 
-           
-            >
+        <div className = "box" onClick={link}>
+            <div className="imageHolder">
                 <img
                     src={props.img}
-                    alt="Hello"           
+                    alt="Hello"
                 />
             </div>
+            <div>
+                
+                <div className="content">
+                    <strong className="title">
+                        {title}
+                    </strong>
+                    <br/>
+                    {ct}
+                </div>
+            </div> 
         </div>
         <style jsx>
             {`
             .box{
+                display: flex;
                 background: rgba(255, 255, 255, 1);
                 box-shadow: 0px 0 10px rgba(0, 0, 0, .1);
+                height: 10rem;
+                border-radius: 0.5rem;
+                
             }
-            .img{
-                //width: 100%;
-                height: 100%;
-                right: 0px;
-                border-radius: 30px;
+            
+            img{
+                width: 10rem;
+                height: 10rem;
+                margin-right: 0px;
+                border-radius: 0.5rem;
+            }
+
+            .title{
+                font-size:1.5em;
+            }
+
+            .content{
+                margin: 1rem;
             }
             
             `}
