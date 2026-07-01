7b1ca218433fe75b82562abe053b8f700558f522
legitminh <totallylegitperson.example@gmail.com>
2026-07-01 17:01:41 +0700

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
