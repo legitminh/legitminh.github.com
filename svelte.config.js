01dcb64963a6dca74b2a862d8c75de4ebcda9285
legitminh <totallylegitperson.example@gmail.com>
2026-07-09 09:37:45 +0700

    fix: copilot fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH ?? '')
 		}
 	}
 };
