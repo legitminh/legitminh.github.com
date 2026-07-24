f1f177de8374170675d91a4825e64a3cc7fe126d
legitminh <totallylegitperson.example@gmail.com>
2026-07-24 17:09:45 -0400

    update: theme toggle

--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import InteractablePositional from "$lib/components/InteractablePositional.svelte";
   import { themes, set_theme_id, theme_id } from "$lib/stores/theme";
+  import ButtonPositional from "./ButtonPositional.svelte";
 
   function toggle_theme() {
     set_theme_id($theme_id === 0 ? 1 : 0);
@@ -9,6 +9,6 @@
   let theme_name = $derived(themes[$theme_id]?.name ?? "unknown");
 </script>
 
-<InteractablePositional on_close={toggle_theme}>
+<ButtonPositional on_close={toggle_theme}>
   {theme_name}
-</InteractablePositional>
+</ButtonPositional>
