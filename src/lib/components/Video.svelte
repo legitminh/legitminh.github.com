91f058c394451ec796e21e7abe3bffbb824c9feb
legitminh <totallylegitperson.example@gmail.com>
2026-08-31 13:02:22 -0400

    add: first video

---
+++ src/lib/components/Video.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let { href } = $props();
+</script>
+
+<video
+  src={href}
+  autoplay
+  muted
+  loop
+  playsinline
+  class="video"
+>
+</video>
+
+<style>
+  .video {
+    width: auto;
+    height: auto;
+    max-width: 75vw;
+    max-height: 75vh;
+    object-fit: contain;
+  }
+</style>
--- src/routes/project/astrofest/+page.svelte
+++ src/routes/project/astrofest/+page.svelte
@@ -3,6 +3,7 @@
   import Link from "$lib/components/Link.svelte";
   import Document from "$lib/components/scroll/vertical/Document.svelte";
   import LineText from "$lib/components/LineText.svelte";
+  import Video from "$lib/components/Video.svelte";
 </script>
 <Document>
   <Link href="/">
@@ -26,4 +27,5 @@
   <LineText>Future updates include art, turrets, maps, sound effect, online multiplayer, and more...</LineText>
   <LineText>Thank you for checking this out.</LineText>
   <LineText>If you want to support me and/or download the game, check out the <Link href="https://minhn.itch.io/astrofest2">itch page</Link>.</LineText>
+  <Video href="/videos/Astrofest1.mp4"></Video>
 </Document>
