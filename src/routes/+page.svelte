3beba221eb731fd1e2afb403f83ae027279d8805
legitminh <totallylegitperson.example@gmail.com>
2026-09-02 15:11:33 -0400

    add projects page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,35 @@
 <script lang="ts">
+  import LineText from '$lib/components/LineText.svelte';
   import { min_box } from '$lib/stores/layout.svelte';
   import ButtonPositional from '$lib/components/ButtonPositional.svelte';
   import ThemeToggle from '$lib/components/ThemeToggle.svelte';
   import Link from '$lib/components/Link.svelte';
   import Grid from '$lib/components/Grid.svelte';
   import Document from '$lib/components/scroll/vertical/Document.svelte';
+  import Indent from '$lib/components/Indent.svelte';
 </script>
 <Document>
   <div class="content">
     <span>
-      website under construction.
+      minh.website
     </span>
     <div style="flex-direction: column; display: flex;">
+    <LineText>settings</LineText>
+    <Indent>
+      <LineText><ThemeToggle></ThemeToggle></LineText>
+
+      <LineText><ButtonPositional on_close={() => {
+        min_box.set(min_box.value * 0.8);
+      }}>zoom out</ButtonPositional></LineText>
+
+      <LineText><ButtonPositional on_close={() => {
+        min_box.set(min_box.value * 1.25);
+      }}>zoom in</ButtonPositional></LineText>
+    </Indent>
     <Link href="/about">about</Link>
 
-    <ButtonPositional on_close={() => {
-      min_box.set(min_box.value * 0.8);
-    }}>zoom out</ButtonPositional>
+    <Link href="/project">project</Link>
 
-    <ButtonPositional on_close={() => {
-      min_box.set(min_box.value * 1.25);
-    }}>zoom in</ButtonPositional>
-
-    <ThemeToggle></ThemeToggle>
     </div>
   </div>
 </Document>
---
+++ src/routes/project/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import LineText from "$lib/components/LineText.svelte";
+import Link from "$lib/components/Link.svelte";
+  import Document from "$lib/components/scroll/vertical/Document.svelte";
+</script>
+<Document>
+  <LineText> <Link href="/"> home </Link> </LineText>
+  <LineText> <Link href="project/astrofest"> astrofest </Link> </LineText>
+</Document>
